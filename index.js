ec10a6a896fcd7b42192029451f3df613afab5ca
Prashant <prashantkumar50@hotmail.com>
2017-10-28 00:48:47 +0530

    Added multifile upload and creation time with comments through out

--- index.js
+++ index.js
@@ -1,48 +1,59 @@
 var express = require("express");
-var app =  express();
-var path = require('path');
-var mime = require('mime');
 var fs = require('fs');
-const fileUpload = require('express-fileupload');
+var multer  = require('multer');
+
+var app =  express();
+
 app.set("view engine","ejs");
-var fileNames = [];
+
+var fileinfo = [];
 var dir = __dirname +"\\files\\";
-var pastfiles;
 
-app.use(fileUpload({
-	safeFileNames : true,
-	preserveExtension : 4
-}));
+//storage for multer helps decide destination and filename
+var storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, dir);
+  },
+  filename: function (req, file, callback) {
+    console.log(file);
+    callback(null, file.originalname);
+  }
+});
 
+var upload = multer({storage: storage}).array('newfile',20);
+
+//home page checks for files in the files folder and passes file info to home.ejs
 app.get("/",function(req,res){
-	console.log(dir);
+	//console.log(dir);
   fs.readdir(dir, function(err, files)
   {
-		fileNames = [];
+		fileinfo = [];
 	  	files.forEach(function(file)
 	     {
 	     	removeSpaces("files/" +file);
-	     	fileNames.push(file);
+				fs.stat(dir + file, function (err, stats) {
+					if(err)
+					console.log("Error while reading file creation time:" + err);
+   			var filedata = {filename: file, filetime: String(stats.birthtime).substring(0,24)};
+				fileinfo.push(filedata);
+				});
+
 	  	 });
 
   });
- 	res.render("home",{fileNames:fileNames});
+	//passing fileinfo to be displayed in home page in the next line
+ 	res.render("home",{fileinfo:fileinfo});
 });
 
+// route to download files
 app.get('/download/:namefile', function(req, res){
-  console.log(req.params.namefile);
+//  console.log("hey man" + req.params.namefile);
   var file = dir + req.params.namefile;
-
-  var filename = path.basename(file);
-  var mimetype = mime.lookup(file);
-
-  res.setHeader('Content-disposition', 'attachment; filename=' + filename);
-  res.setHeader('Content-type', mimetype);
-
   var filestream = fs.createReadStream(file);
   filestream.pipe(res);
 });
 
+// function to remove spaces from file names
 function removeSpaces(fil){
 	var newname = fil.replace(" ","_");
 	fs.rename(fil, newname, function(err) {
@@ -50,17 +61,20 @@ function removeSpaces(fil){
 });
 }
 
+//route to upload files. It used upload method defined above to upload the files via multer
 app.post("/uploadfile",function(req,res){
-	if (!req.files)
-    return res.status(400).send('No files were uploaded.');
+	upload(req, res, function(err) {
+	if(err) {
+		console.log('Error Occured ' + err);
+		return;
+	}
+	console.log(req.files);
+	res.end('Your Files Uploaded');
+	console.log('Photo Uploaded');
+})
 
-	let newfile = req.files.newfile;
-	console.log(dir + " " +newfile);
-	newfile.mv(dir+newfile.name, function(err){
-		if(err)
-			return res.status(500).send(err);
-	res.send("file uploaded");
-	});
+
+
 });
 
 app.listen(3000, function(){
